09ca84d6bb14b8f7c427dff0a85d7b7049dd84cd
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-05-15 10:51:41 +0300

    Add files via upload

---
+++ Delegate/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        int n = 10;
+
+        public delegate void Number(int luku);
+
+        public decimal addOne(Number number)
+        {
+            number(n);
+
+            n++;
+
+            return n;
+        }
+    }
+}
---
+++ Delegate/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Class1 olio = new Class1();
+        
+
+        private static void addAlert(int luku2)
+        {
+            Console.WriteLine($"Alkuperäinen {luku2}");
+        }
+
+        private static void addAlert2(int luku2)
+        {
+            Console.WriteLine($"Alkuperäinen {luku2+5}");
+        }
+
+        static void Main()
+        {
+            //yhdenrivin ratkaisu
+            Console.WriteLine($"Class1 luku on {olio.addOne(addAlert)}");
+            //kaksi riviä
+            decimal luku = olio.addOne(addAlert2);
+            Console.WriteLine(luku);
+            
+            Console.ReadKey();
+        }
+
+       
+    }
+}
