e6d5486a2b4f42449619d717c0adb12011844066
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-05-15 11:46:48 +0300

    Add files via upload

---
+++ Lamba_expression/Lamba_expression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba_expression
+{
+    class Program
+    {
+        public delegate void p(int value);
+
+        static int a = 2;
+        static void Main(string[] args)
+        {
+            p p2 = a => Console.WriteLine(a);
+
+            p2(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lamba_expression/Lamba_expression/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamba_expression
+{
+    class Test
+    {
+        public delegate void p2(int value);
+        int a = 2;
+    }
+}
