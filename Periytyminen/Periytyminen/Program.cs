4f45461d0fc13c0c9bc687ce3a4dcc672b74dbfb
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 09:58:29 +0300

    Add files via upload

---
+++ Periytyminen/Periytyminen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Periytyminen
+{
+    public class CarCollection 
+    {
+        public string car = "Volvo";
+    }
+    class Program : CarCollection
+    {
+        static void Main(string[] args)
+        {
+            string car;
+            CarCollection c = new CarCollection();
+            Program p = new Program();
+
+            car = c.car;
+            Console.WriteLine(car);
+            car = p.car;
+            Console.WriteLine(car);
+            Console.ReadKey();
+        }
+    }
+}
