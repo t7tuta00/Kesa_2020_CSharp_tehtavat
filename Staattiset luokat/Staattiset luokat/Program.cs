74dbcb777d4e7e6842c6190e9ffe46b98d4a3c53
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 10:00:36 +0300

    Add files via upload

---
+++ Staattiset luokat/Staattiset luokat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staattiset_luokat
+{
+    public static class CarCollection 
+    {
+        static string car = "Volvo";
+
+        public static string getCar() 
+        {
+            return car;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string car;
+            car = CarCollection.getCar();
+            Console.WriteLine(car);
+            Console.ReadKey();
+        }
+    }
+}
