76f32ef29b23e07481022070ff7a0139162d96ac
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 08:02:51 +0300

    Add files via upload

---
+++ Absraktit luokat ja jäsenet/Absraktit luokat ja jäsenet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absraktit_luokat_ja_jäsenet
+{
+    public abstract class Program 
+    {
+        public abstract void ExampleMethod();
+    }
+    class Program2 : Program
+    {
+        public override void ExampleMethod()
+        {
+            Console.WriteLine("Doing stuff");
+        }
+
+        static void Main(string[] args)
+        {
+            Program2 p = new Program2();
+            p.ExampleMethod();
+            Console.ReadKey();
+        }
+
+        
+    }
+}
