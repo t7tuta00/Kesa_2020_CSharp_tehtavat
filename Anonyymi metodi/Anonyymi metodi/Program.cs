e898b401bbaeb8f3f6aeca6fbf3a1413ac6fc047
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 12:49:04 +0300

    Add files via upload

---
+++ Anonyymi metodi/Anonyymi metodi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonyymi_metodi
+{
+    public class Program 
+    {
+        public delegate void printDelegate(string message);
+        public printDelegate print;
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.print += delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+
+            p.print("Hello World");
+            Console.ReadKey();
+        }
+        
+    }
+}
