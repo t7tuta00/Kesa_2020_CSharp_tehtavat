336057807909d5ae5b906b65790e13ffc115b499
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 08:42:39 +0300

    Add files via upload

---
+++ Muodostimet/Muodostimet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muodostimet
+{
+    public class CarCollection 
+    {
+        string car;
+        string color;
+
+        public CarCollection(string a, string b) 
+        {
+            car = a;
+            color = b;
+        }
+
+        public void getCarAndColor() 
+        {
+            Console.WriteLine(car+color);
+        }
+}
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string car = "Volvo";
+            string color = "Red";
+            CarCollection c = new CarCollection(car,color);
+
+            c.getCarAndColor();
+            Console.ReadKey();
+        }
+    }
+}
