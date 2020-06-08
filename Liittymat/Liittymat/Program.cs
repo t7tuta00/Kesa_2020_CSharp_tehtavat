96a67cfa70ad12ad9f08e43c1cb6944beaae7391
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 07:57:49 +0300

    Add files via upload

---
+++ Liittymat/Liittymat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liittymat
+{
+    interface ISampleInterface 
+    {
+        void SampleMethod();
+    }
+    class Program : ISampleInterface
+    {
+        void ISampleInterface.SampleMethod()
+        {
+            Console.WriteLine("Fill interface");
+        }
+
+        static void Main(string[] args)
+        {
+            ISampleInterface p = new Program();
+            p.SampleMethod();
+            Console.ReadKey();
+        }
+
+        
+    }
+}
