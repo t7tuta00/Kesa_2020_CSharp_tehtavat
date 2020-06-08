757bae3f3b2af4cb7281ef1c4e485e7ff6d9f859
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 08:54:41 +0300

    Add files via upload

---
+++ Osittaminen/Osittaminen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osittaminen
+{
+    public partial class Program
+    {
+        public int n;
+        public int getsetn
+        {
+            get
+            {
+                return n;
+            }
+            set
+            {
+                n = value;
+            }
+        }
+
+        public int getInt{get; set;}
+    }
+
+    public partial class Program
+    {
+        public int asd() 
+        {
+            return getInt;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.getsetn = 5;
+            p.getInt = 2;
+            Console.WriteLine(p.asd());
+            Console.WriteLine(p.n);
+            Console.ReadKey();
+        }
+    }
+}
