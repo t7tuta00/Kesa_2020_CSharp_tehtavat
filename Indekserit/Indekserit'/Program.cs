deb6cece7fd0248e8f53cc427872a695a41738a4
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 08:35:55 +0300

    Add files via upload

---
+++ Indekserit/Indekserit'/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indekserit_
+{
+    public class Collection<T> 
+    {
+        private T[] arr = new T[100];
+
+        public T this[int i] 
+        {
+            get { return arr[i]; }
+            set { arr[i] = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringCollection = new Collection<string>();
+            stringCollection[0] = "Hello World";
+            Console.WriteLine(stringCollection[0]);
+            Console.ReadKey();
+        }
+    }
+}
