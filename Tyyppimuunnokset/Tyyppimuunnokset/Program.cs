17813fc7085cd520be661ad5dfd1e01eba16c79b
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 12:39:46 +0300

    Add files via upload

---
+++ Tyyppimuunnokset/Tyyppimuunnokset/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyyppimuunnokset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float floatNumber = 10.5f;
+            int intNumber = (int)floatNumber;
+            Console.WriteLine(intNumber);
+
+            string stringText = "100a";
+            int number;
+            if (int.TryParse(stringText, out number))
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("fail");
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
