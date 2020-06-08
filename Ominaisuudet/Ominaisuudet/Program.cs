fb2813de449b37c84003f4df6ec1a65c8a7e4c01
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-08 08:47:26 +0300

    Add files via upload

---
+++ Ominaisuudet/Ominaisuudet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ominaisuudet
+{
+    public class CarCollection 
+    {
+        private string car;
+        public string Car 
+        {
+            get { return car; }
+            set { car = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarCollection c = new CarCollection();
+            c.Car = "Volvo";
+            Console.WriteLine(c.Car);
+            Console.ReadKey();
+        }
+    }
+}
