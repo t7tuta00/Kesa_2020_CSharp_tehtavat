cc0abd74d22f46b0cbed3ac99e59242b29e8dd12
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-09 08:38:14 +0300

    Add files via upload

---
+++ Operaattorien kuormitus/Operaattorien kuormitus/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaattorien_kuormitus
+{
+    public class Coffee 
+    { 
+        public static MilkCoffee operator +(Coffee coffee, Milk milk) 
+        {
+            return new MilkCoffee(coffee, milk);
+        }
+    }
+
+    public class Milk 
+    {
+    }
+
+    public class MilkCoffee 
+    {
+        private Coffee coffee;
+        private Milk milk;
+
+        public MilkCoffee(Coffee coffee, Milk milk) 
+        {
+            this.coffee = coffee;
+            this.milk = milk;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coffee coffee = new Coffee();
+            Milk milk = new Milk();
+            MilkCoffee milkcoffee = coffee + milk;
+
+        }
+    }
+}
