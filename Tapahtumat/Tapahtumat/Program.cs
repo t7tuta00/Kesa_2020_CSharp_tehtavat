786f793c0d6d3dc9550a78a68da753dee96a2dae
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-09 09:03:13 +0300

    Add files via upload

---
+++ Tapahtumat/Tapahtumat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapahtumat
+{
+   public class Process 
+    {
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess() 
+        {
+            Console.WriteLine("Process started!");
+            OnProcessCompleted(EventArgs.Empty);
+        }
+
+        protected virtual void OnProcessCompleted(EventArgs e) 
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            p.ProcessCompleted += p_ProcessCompleted;
+            p.StartProcess();
+            Console.ReadKey();
+        }
+
+        public static void p_ProcessCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Completed");
+        }
+    }
+}
