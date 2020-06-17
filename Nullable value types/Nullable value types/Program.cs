79fb1368562bd2f2b4c3c76dd59bd818d0a98030
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-17 09:58:19 +0300

    Add files via upload

---
+++ Nullable value types/Nullable value types/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable_value_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? pi = null;
+            char? kirjain = 'a';
+            int m2 = 10;
+            int? m = m2;
+            bool? flag = null;
+            int?[] array = new int?[10];
+
+            if (pi is double valueOfA) 
+            {
+                Console.WriteLine(pi);
+            }
+            else
+            {
+                Console.WriteLine("sisältö null");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
