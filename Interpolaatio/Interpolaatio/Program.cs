923be9a8ad3cafa0dff1895ae42c8abdfd2c4e79
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-22 12:48:39 +0300

    Add files via upload

---
+++ Interpolaatio/Interpolaatio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolaatio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Taneli";
+            var date = DateTime.Now;
+
+      
+            Console.WriteLine("Hello, {0},{1},{2:HH:mm}", name, date.DayOfWeek, date);
+            
+            Console.WriteLine($"Hello, {name},{date.DayOfWeek},{date:HH:mm}");
+            
+            Console.ReadKey();
+        }
+    }
+}
