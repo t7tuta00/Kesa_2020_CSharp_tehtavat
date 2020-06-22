eed52752669d67959959ca8957045796cb4fd8c5
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-22 12:20:32 +0300

    Add files via upload

---
+++ Iterators/Iterators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int numero in EvenSequence(5,18))
+            {
+                Console.Write(numero.ToString() + " ");
+            }
+            
+            Console.ReadKey();
+        }
+
+        public static System.Collections.Generic.IEnumerable<int>
+        EvenSequence(int firstNumber, int lastNumber)
+        {
+            for (int number = firstNumber; number <= lastNumber; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    yield return number;
+                }
+            }
+        }
+    }
+}
