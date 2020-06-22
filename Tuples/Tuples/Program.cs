ab7abd707166bbe821355385eccb39909a5673e5
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2020-06-22 13:01:22 +0300

    Add files via upload

---
+++ Tuples/Tuples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuples
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var numbers = Tuple.Create("One", 2, 3, "Four", 5, "Six", 7, 8);
+            Console.WriteLine(numbers.Item1);
+            Console.WriteLine(numbers.Item2);
+            Console.WriteLine(numbers.Item3);
+            Console.WriteLine(numbers.Item4);
+            Console.WriteLine(numbers.Item5);
+            Console.WriteLine(numbers.Item6);
+            Console.WriteLine(numbers.Item7);
+            Console.WriteLine(numbers.Rest);
+            Console.WriteLine(numbers.Rest.Item1);
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
